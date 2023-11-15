ce274d0a1bcd4c4bfb49086eb082ccc43e48c429
Alexey <boronin_1993@mail.ru>
2023-11-15 20:14:15 +0300

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Diagnostics;
+
+namespace LogTo
+{
+    public class ApplicationContext:DbContext
+    {
+        readonly StreamWriter logSt = new StreamWriter("mylog.txt", true);
+        public DbSet<Human> Humans { get; set; } = null!;
+        public ApplicationContext() 
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data source=Hello.db");
+            optionsBuilder.LogTo(logSt.WriteLine);
+        }
+        public override void Dispose()
+        {
+            base.Dispose();
+            logSt.Dispose();
+        }
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await logSt.DisposeAsync();
+        }
+    }
+}
+
+/*LogTo()
+Trace
+Debug
+Information
+Warning
+Error
+Critical
+None
+
+SqlServerEventId
+CoreEventId
+RelationalEventId
+
+DbLoggerCategory
+
+Database.Command
+Database.Connection
+Database.Transaction
+Mirgation
+Model
+Query
+Scaffolding
+Update
+Infrastructure*/
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using LogTo;
+using (ApplicationContext db = new ApplicationContext())
+{
+    Human h1 = new Human { Name = "Ted", Age = 30 };
+    Human h2 = new Human { Name = "Jane", Age = 25 };
+    
+    db.Humans.Add(h1);
+    db.Humans.Add(h2);
+    db.SaveChanges();
+
+    var humans = db.Humans.ToList();
+    Console.WriteLine("Список пользователей:");
+    foreach (var h in humans)
+        Console.WriteLine($"{h.Id}.{h.Name} - {h.Age}");
+
+}
